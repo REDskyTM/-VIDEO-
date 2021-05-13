6c87a139e0dcf05f917677689792823f72b592bf
REDskyTM <71713652+REDskyTM@users.noreply.github.com>
2021-05-13 15:56:55 +0600

    Изменил не множко

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ async def on_ready():
 
 @client.command()
 async def load(ctx, extension):
-	if ctx.author.id == 745654846220271637:
+	if ctx.author.id == ВАШ АЙДИ:
 		client.load_extension(f"cogs.{extension}")
 		await ctx.send("Ког загружен")
 	else:
@@ -18,7 +18,7 @@ async def load(ctx, extension):
 
 @client.command()
 async def reload(ctx, extension):
-	if ctx.author.id == 745654846220271637:
+	if ctx.author.id == ВАШ АЙДИ:
 		client.unload_extension(f"cogs.{extension}")
 		client.load_extension(f"cogs.{extension}")
 		await ctx.send("Ког загружен")
@@ -27,7 +27,7 @@ async def reload(ctx, extension):
 
 @client.command()
 async def unload(ctx, extension):
-	if ctx.author.id == 745654846220271637:
+	if ctx.author.id == ВАШ АЙДИ:
 		client.unload_extension(f"cogs.{extension}")
 		await ctx.send("Ког загружен")
 	else:
@@ -37,4 +37,4 @@ for filename in os.listdir("./cogs"):
 	if filename.endswith(".py"):
 		client.load_extension(f"cogs.{filename[:-3]}")
 
-client.run('ODM0ODUyNTg5NjU2MzQyNTM4.YIG7EA.7C5dLOjMNLFJ9PXa-2S7kh2SE9A')
+client.run('Ваш токен')
